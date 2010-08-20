1d5c0fc323c94c5c600b31d164d4d9baa0f2b445
mat_aki <matsumura.aki@gmail.com>
2010-08-20 19:34:48 +0900

    Revert "Add url to post on youRoom"
    This reverts commit 740871669bb093bae9a4cc0a70504f8a07d5d556.
    Add url to post on youRoom
    Reverting "Add url to post on youRoom"
    
    URL is API's url not User access url

--- application.rb
+++ application.rb
@@ -16,14 +16,13 @@ post "/" do
   consumer = OAuth::Consumer.new(consumer_token, consumer_secret, :site => "https://www.youroom.in")
   access = OAuth::AccessToken.new(consumer, access_token, access_secret)
 
-  doc = Hpricot(request.body.read)
-  content = CGI.unescapeHTML(doc.at('activity').at('description').innerHTML)
-  url = CGI.unescapeHTML(doc.at('activity').at('stories').at('story').at('url').innerHTML)
+  doc = Hpricot(CGI.unescapeHTML(request.body.read))
+  content = doc.at('activity').at('description').innerHTML
 
   logger = env['rack.errors']
   logger.write request.body.read
-  post_data = { "entry[content]" => "#pivotal\n\n#{content}", "entry[attachment_attributes][attachment_type]" => "Link", "entry[attachment_attributes][data][url]" => url }
-  resp = access.post("https://www.youroom.in/r/#{room_id}/entries?format=json", post_data)
+
+  resp = access.post("https://www.youroom.in/r/#{room_id}/entries?format=json", { "entry[content]" => "#pivotal\n\n#{content}" })
   logger.write "-" * 30
   logger.write resp.to_s
   resp
